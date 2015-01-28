1a9a1c3c8c0fe53618328907848640a589fd57d2
Nicholai Goodall <kranoscorp@gmail.com>
2015-01-28 20:41:08 +0000

    added dragdealer for input and moved input methods from scaffolding to implementation

--- scripts/script.js
+++ scripts/script.js
@@ -1,58 +1,45 @@
-var fileInput = $('#files');
-var uploadButton = $('#upload');
+$(function() {
+    var fileInput = $('#files');
+    var uploadButton = $('#upload');
 
-function generateMatrix(Nx, Ny) {
+    function generateMatrix(Nx, Ny) {
 
-}
-
-function processMatrix(normMatrix) {
-
-}
-
-function iterateJacobi(m) {
-
-}
-
-function iterateGauss(m) {
-
-}
-
-
-function processFile(e) {
-    var file = e.target.result,
-        results;
-    if (file && file.length) {
-        results = file.split("\n");
-        console.log(results)
-        $('#name').val(results[0]);
-        $('#age').val(results[1]);
     }
-}
 
-uploadButton.on('click', function() {
-    if (!window.FileReader) {
-        alert('Your browser is not supported')
+    function processMatrix(normMatrix) {
+
     }
-    var input = fileInput.get(0);
 
-    // Create a reader object
-    var reader = new FileReader();
-    if (input.files.length) {
-        if (input.files.length === 2) {
-            var textFile1 = input.files[0];
-            var textFile2 = input.files[1];
-            reader.readAsText(textFile1);
-            var txt1 = $(reader).on('load', processFile);
-            reader.readAsText(textFile2);
-            var txt2 = $(reader).on('load', processFile);
-            // MAIN FUNCTION CALL
-        } else {
-            alert('Please make sure to upload two text files.')
+    function iterateJacobi(m) {
+
+    }
+
+    function iterateGauss(m) {
+
+    }
+
+    new Dragdealer('x-slider', {
+        steps: 8,
+        animationCallback: function(x,y){
+            $('#x-slider .value').text((x*8)+2);
         }
-    } else {
-        alert('Please upload a file before continuing.')
-    }
+    });
+
+    new Dragdealer('y-slider', {
+        steps: 8,
+        animationCallback: function(x,y){
+            $('#x-slider .value').text((x*8)+2);
+        }
+    });
+
+    var genButton = $('#generate')
+
+    genButton.on('click', function() {
+        var x = $('x-slider .value').innerHTML;
+        var y = $('y-slider .value').innerHTML;
+        var origMatrix = generateMatrix(x, y);
+    });
+
+
+
 });
-
-
-
